aac8626c6a63294427dfeaff37a57f52089c2fab
planetabhi <abhimanyu.rana@postman.com>
2024-08-16 02:38:42 +0530

    setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# Journal 
+Astro blog template.
+
+- Built with Astro
+- Light & dark themes
+- Production-ready
+- Simple to deploy
+
+### Guides
+- [Getting started guide](https://new-ui.com/docs/templates/journal)
--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://new-ui.com/templates/blog",
+  site: "https://new-ui.com/templates/journal",
   trailingSlash: "never",
   prefetch: {
     prefetchAll: true,
